75690e20f94ffb62f80b93d3ba06ba1756b2fb08
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-14 12:37:02 +0200

    add Day15_14.06_JS-Basics Ex1_12

---
+++ Day15_14.06_JS-Basics/Ex1_12/assets/js/script.js
@@ -0,0 +1,16 @@
+let score = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+let score2 = (5 + 5) * 10;
+console.log("Ergebnis: " + score2);
+
+let score3 = 0;
+score3 = score3 + 10;
+console.log("Ergebnis: " + score3);
+
+let zahl = 0;
+zahl++;
+console.log("increment: " + zahl);
+
+zahl--;  
+console.log('dekrement: ' + zahl);
