ca82407e8a49b52117ce33bb2ea914d04d364e11
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-14 12:31:18 +0200

    add Day15_14.06_JS-Basics Ex1_1 and Ex1_7

---
+++ Day15_14.06_JS-Basics/Ex1_7/assets/js/script.js
@@ -0,0 +1,24 @@
+let helloworld = "Hello World";
+console.log(helloworld);
+helloworld += " I am Vittorio";
+console.log(helloworld);
+
+const pi = 3.14;
+console.log(pi);
+
+const burjdubai = "828m";
+console.log(burjdubai);
+
+let fullname = "Jan Schmidt";
+console.log(fullname);
+
+const eifeltower = "324m";
+console.log(eifeltower);
+
+let camelCase = "camelCase";
+console.log(camelCase);
+
+let kursStatus = true;
+console.log("kurs status: " + kursStatus);
+kursstatus = false;
+console.log("kurs status: " + kursStatus);
