4f4a3b425c789fbbd894354aa22cee6d0de018d9
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-17 15:34:42 +0200

    add Day16_JS_Functions_Ex1_5

---
+++ Day16_17.06_JS-Basics_Vergleichesoperatoren_DOM-Manipulation_innerHTML_Stylen-JS_Functions/Functions/Ex1_5/js/scrypt.js
@@ -0,0 +1,12 @@
+function math(num1, num2) {
+  console.log(`Multiplication of ${num1} and ${num2}: ${num1 * num2}`);
+  console.log(`Division of ${num1} and ${num2}: ${num1 / num2}`);
+}
+
+
+math(10, 2);
+math(30, 20);
+math(100, 100);
+math(5, 0);
+math(45, 173);
+math(1, 1000);
