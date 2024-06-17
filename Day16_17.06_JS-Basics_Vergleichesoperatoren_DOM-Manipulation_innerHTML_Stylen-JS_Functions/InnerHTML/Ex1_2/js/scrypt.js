71b78e1b0d722cc87ccfaa4decc04639b3937fd7
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-17 14:31:32 +0200

    add Day16_JS_InnerHTML_Ex1_2

---
+++ Day16_17.06_JS-Basics_Vergleichesoperatoren_DOM-Manipulation_innerHTML_Stylen-JS_Functions/InnerHTML/Ex1_2/js/scrypt.js
@@ -0,0 +1,6 @@
+document.getElementById("container").innerHTML = "<p>start of the element</p>";
+
+document.querySelector("#info").innerHTML = "<h1>Hello World</h1>";
+document.getElementById("info").innerHTML += "<h2>How are you?</h2>";
+
+document.write("<p>end of the element</p>");
