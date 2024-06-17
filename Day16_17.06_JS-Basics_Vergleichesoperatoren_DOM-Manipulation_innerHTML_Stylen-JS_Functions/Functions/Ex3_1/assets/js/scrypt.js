a3134777881ae45712a6b2f451ccc95b5ee84076
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-17 16:13:42 +0200

    add Day16_JS_Functions_Ex3_1

---
+++ Day16_17.06_JS-Basics_Vergleichesoperatoren_DOM-Manipulation_innerHTML_Stylen-JS_Functions/Functions/Ex3_1/assets/js/scrypt.js
@@ -0,0 +1,13 @@
+function btn1style() {
+  document.body.style.backgroundColor = "green";
+}
+function btn2style() {
+  document.body.style.backgroundColor = "rgb(0, 64, 255)";
+}
+function btn3style() {
+  document.body.style.backgroundColor = "rgb(0, 0, 0)";
+  document.body.querySelector("h1").style.color = "white";
+}
+function btn4style() {
+  document.body.style.backgroundColor = "rgb(0, 217, 255)";
+}
