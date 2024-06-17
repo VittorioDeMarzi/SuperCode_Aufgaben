8c3a69f07561c111e4013f82decf7847a8781f34
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-17 14:57:23 +0200

    add Day16_JS_InnerHTML_Ex1_3

---
+++ Day16_17.06_JS-Basics_Vergleichesoperatoren_DOM-Manipulation_innerHTML_Stylen-JS_Functions/InnerHTML/Ex1_3/js/scrypt.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#gallery");
+
+container.innerHTML =
+  "<figure><img src='./img/frank-huang-7fMiXiikWQY-unsplash.jpg' style='width: 200px;'/><figcaption>Fig.1</figcaption></figure > ";
+container.innerHTML +=
+  "<figure><img src='./img/frank-huang-7fMiXiikWQY-unsplash.jpg' style='width: 200px;'/><figcaption>Fig.2</figcaption></figure > ";
+container.innerHTML +=
+  "<figure><img src='./img/frank-huang-7fMiXiikWQY-unsplash.jpg' style='width: 200px;'/><figcaption>Fig.3</figcaption></figure > ";
