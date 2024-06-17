8e0f72be1990156be1df9ec0854d8173e15c2842
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-06-17 15:18:14 +0200

    add Day16_JS_Functions_Ex1_1

---
+++ Day16_17.06_JS-Basics_Vergleichesoperatoren_DOM-Manipulation_innerHTML_Stylen-JS_Functions/Functions/Ex1_2/js/scrypt.js
@@ -0,0 +1,13 @@
+function intro2(name) {
+  console.log(`Hi ${name}. Mein Name ist xyz`);
+  }
+  
+let name = "SuperCoder:in";
+intro2(name);
+
+name="Vittorio"
+intro2(name);
+name="Deng"
+intro2(name);
+name="Marzio"
+intro2(name);
